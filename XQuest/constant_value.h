012e99b98c23335318a0edb94cd3ad0f7434bd00
NguyenTrungHieu <21020017@vnu.edu.vn>
2022-03-11 15:49:36 +0700

    Add Constant_value

---
+++ XQuest/constant_value.h
@@ -0,0 +1,29 @@
+#ifndef CONSTANT_VALUE__H
+#define CONSTANT_VALUE__H
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int LEVEL_HEIGHT = 1920;
+const int LEVEL_WIDTH = 1280;
+const int TOTAL_TILES = 384;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const int TOTAL_TILE_SPRITES = 35;
+const int TILE_WIDTH = 80;
+const int TILE_HEIGHT = 80;
+const string WINDOW_TITLE = "Megaman Quest";
+const int CHAR_WIDTH = 60;
+const int CHAR_HEIGHT = 60;
+const int CHAR_VEL = 5;
+const int CHAR_JUMP = 300;
+const int CHAR_DASH = 300;
+const int DIAMOND_WIDTH = 40;
+const int DIAMOND_HEIGHT = 40;
+const int PORTAL_HEIGHT = 200;
+const int PORTAL_WIDTH = 80;
+const int TOTAL_DIAMONDS = 20;
+const int BUTTON_WIDTH = 90;
+const int BUTTON_HEIGHT = 90;
+#endif // CONSTANT_VALUE__H
